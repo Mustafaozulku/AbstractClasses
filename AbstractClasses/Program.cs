889fbd780032ca72b06084088613a92dcbecf479
Damla <Damla@LAPTOP-OFAHLJQ9>
2022-12-15 14:57:53 +0300

    Add project files.

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AMevzuat aMevzuat = new AMevzuat();
+            aMevzuat.Değerlendir();
+            aMevzuat.Kaydet();
+            BMevzuant bMevzuant = new BMevzuant();
+            bMevzuant.Değerlendir();
+            bMevzuant.Kaydet();
+        }
+    }
+    abstract class MevzuatBase
+    {
+        public abstract void Değerlendir();
+
+        public void Kaydet()
+        {
+            Console.WriteLine("kaydedildi");
+        }
+      
+    }
+    class AMevzuat : MevzuatBase
+    {
+        public override void Değerlendir()
+        {
+            Console.WriteLine("A Mevzuatına göre değerlendir");
+        }
+    }
+    class BMevzuant : MevzuatBase
+    {
+        public override void Değerlendir()
+        {
+            Console.WriteLine("B Mevzuatına göre Değerlendir");
+        }
+    }
+}
